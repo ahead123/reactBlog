d1f569715a10a2f65b3b1872336ffaba484e47df
ahead123 <arthurallenhead@yahoo.com>
2017-11-22 15:05:07 -0800

    updated production constants for heroku push

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-const dev = true;
+const dev = false;
 export const POSTS_ENDPOINT = dev ? 'http://localhost:3000/posts' : 'https://blogreactified.herokuapp.com/posts'; 
 export const SUBSCRIBE_ENDPOINT = dev ? 'http://localhost:3000/subscribe' : 'https://blogreactified.herokuapp.com/subscribe';
 export const REGISTER_ENDPOINT = dev ? 'http://localhost:3000/register' : 'https://blogreactified.herokuapp.com/register';
